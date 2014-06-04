3999c140f3e899ebf105af6a95a5bd08c4df20a2
clemeno <clemeno@gmail.com>
2014-06-04 16:37:32 +0900

    Gruntfile warning fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -205,7 +205,6 @@ module.exports	=	function(	grunt	)	{
 	grunt.loadNpmTasks(	'grunt-contrib-qunit'		);
 	grunt.loadNpmTasks(	'grunt-mocha-cli'				);
 	grunt.loadNpmTasks(	'grunt-contrib-jshint'	);
-	grunt.loadNpmTasks(	'grunt-contrib-watch'		);
 
 
 
