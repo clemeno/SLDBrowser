fdb708f48ac40ee0e7516a254a0d684701f3a977
clemeno <clemeno@gmail.com>
2014-06-06 13:33:45 +0900

    ; fix

--- webapp.loading.visuals.js
+++ webapp.loading.visuals.js
@@ -19,7 +19,6 @@
 //	display	elements
 var	img_loading_dark_32	=	'<img	src="img/loading_in_the_dark_002.gif"	'
 			+	'alt="Chargement…"	title="Loading…"	width="24px"	height="24px"	/>'
-			;
 ,		img_valid_32	=	'<img	src="img/valid.png"	alt="Valide"	title="Ok"	width="24px"	height="24px"	/>'
 ,		img_wrong_32	=	'<img	src="img/wrong.png"	alt="Erreur"	title="Wrong"	width="24px"	height="24px"	/>'
 ,		img_images_32	=	'<img	src="img/loadbar001.gif"	alt="Images…"	title="Images…"	width="24px"	height="24px"	/>'
